a83132f0aa07702147a55e363a5e651c80986c30
Dishant Giri <dishantgiri22@gmail.com>
2026-03-10 16:33:24 +0545

    fixed cors error

--- ecom/src/main/java/fishtail/ecom/config/SecurityConfig.java
+++ ecom/src/main/java/fishtail/ecom/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://209.126.86.149:3083"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
