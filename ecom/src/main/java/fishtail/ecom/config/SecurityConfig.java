59d60bd5cec3aa09a2047fc10d49502eb9f89c8e
Dishant Giri <dishantgiri22@gmail.com>
2026-04-30 15:28:12 +0545

    feat: implement Spring Security configuration with JWT authentication and CORS settings

--- ecom/src/main/java/fishtail/ecom/config/SecurityConfig.java
+++ ecom/src/main/java/fishtail/ecom/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://209.145.62.242:3083", "http://localhost:3083", "https://healthcaredrugstore.com", "https://www.healthcaredrugstore.com"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://209.145.62.242:3083", "http://localhost:3083", "https://healthcaredrugstore.com", "https://www.healthcaredrugstore.com", "https://healthcaredrugstore.com/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
