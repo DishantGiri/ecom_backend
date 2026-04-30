a0803d0e6258c1d901d3e128dd962eb5fb7dafcd
Dishant Giri <dishantgiri22@gmail.com>
2026-04-30 12:53:19 +0545

    fix: allow all headers for CORS to avoid preflight issues

--- ecom/src/main/java/fishtail/ecom/config/SecurityConfig.java
+++ ecom/src/main/java/fishtail/ecom/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3000", baseUrl));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
